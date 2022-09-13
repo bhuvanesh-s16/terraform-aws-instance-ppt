4b8efc73ff091b0619777b58028ecbb5dbde8cea
bhuvanesh-s16 <110822412+bhuvanesh-s16@users.noreply.github.com>
2022-09-13 15:34:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc-ppt" {
   source  = "app.terraform.io/module_practice/vpc-ppt/aws"
-  version = "1.0.1"
+  version = "1.0.0"
   # insert required variables here
   vpc_cidr = var.vpc_cidr
   subnet_cidr = var.subnet_cidr
